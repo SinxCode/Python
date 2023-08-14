8eeddefa7f70842d720ea212034824c7ebfe7205
GabrielJBrito <gabriel.jbrito2@gmail.com>
2023-08-14 11:31:28 -0300

    Comparando 2 valores

---
+++ EX038.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite o primeiro número inteiro: '))
+n2 = int(input('Digie o segundo número inteiro: '))
+if n1 > n2:
+    print('O primeiro valor é o maior')
+elif n2 > n1:
+    print('O segundo valor é o maior')
+else:
+    print('Não existe valor maior, os dois são iguais.')
