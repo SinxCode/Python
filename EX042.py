4270d5d7d9d2f5796104288da51b5e84d8579502
GabrielJBrito <gabriel.jbrito2@gmail.com>
2023-08-14 12:10:40 -0300

    triangulos

---
+++ EX042.py
@@ -0,0 +1,17 @@
+print('-='*20)
+print('Analisador de triângulos')
+print('-='*20)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR um triângulo! ', end='')
+    if r1 == r2 and r1 == r3:
+        print('Equilátero')
+    elif r1 != r2 != r3 != r1:
+     print('ESCALENO')
+    else:
+     print('ISÓCELES')  
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo')
+  
