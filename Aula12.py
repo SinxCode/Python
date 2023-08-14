73469dfa00f8e08f873e812f1c85b6284f8d0242
GabrielJBrito <gabriel.jbrito2@gmail.com>
2023-08-14 10:35:13 -0300

    Condições Aninhadas

---
+++ Aula12.py
@@ -0,0 +1,6 @@
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Sinx':
+    print('Que nome foda, {}'.format(nome))
+elif nome == 'Gabriel' or nome =='Pedro' or nome =='Rafael':
+    print('Seu nome é bem comum!')
+else: print('Ok... bom dia!')
