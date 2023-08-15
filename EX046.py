ec5f3814d014cb60ff38704a5bf6a499e35a4cbb
GabrielJBrito <gabriel.jbrito2@gmail.com>
2023-08-15 09:14:14 -0300

    REGRESSIVA PARA O ANO NOVO

---
+++ EX046.py
@@ -0,0 +1,10 @@
+from time import sleep
+print('-='*20)
+string = 'REGRESSIVA PARA O ANO NOVO!'
+print(string.center(42))
+print('-='*20)
+for c in range (10, 1-1, -1):
+    print(c)
+    sleep(1)
+print('FELIZ ANO NOVO!!')
+   
